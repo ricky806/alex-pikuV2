82d18d92afcde79e8908ca2d619c40f196f45b17
ricky806 <117099405+ricky806@users.noreply.github.com>
2024-05-20 16:00:35 +0530

    Update support.js

--- plugins/support.js
+++ plugins/support.js
@@ -3,13 +3,13 @@ const { getJson, getBuffer, command, isPrivate, sleep } = require("../lib/");
 command({
     pattern: "help",
     fromMe: isPrivate,
-    desc: "Anjan support",
+    desc: "À𝖑è𝖝 𝕻ì𝖐ù support",
     type: "support"
 }, async (message) => {
-    const name = '〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»🎈', title = "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢» 𝙎𝙐𝙋𝙋𝙊𝙍𝙏🦋", number = '919883457657', body = "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»";
+    const name = 'À𝖑è𝖝 𝕻ì𝖐ùː͢»🎈', title = "À𝖑è𝖝 𝕻ì𝖐ùː͢» 𝙎𝙐𝙋𝙋𝙊𝙍𝙏🦋", number = '918536881026', body = "À𝖑è𝖝 𝕻ì𝖐ùː͢»";
     const image = "https://i.imgur.com/V2DZbd7.jpeg", sourceUrl = 'https://chat.whatsapp.com/HBf3zvaGyRaEfMZlKgUSPV';
     const logo = await getBuffer(image);
-    const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
+    const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 À𝖑è𝖝 𝕻ì𝖐ùː͢»;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
     const adon = { title, body, thumbnail: logo, mediaType: 1, mediaUrl: sourceUrl, sourceUrl, showAdAttribution: true, renderLargerThumbnail: false };
     await message.client.sendMessage(message.jid, { contacts: { displayName: name, contacts: [{ vcard }] }, contextInfo: { externalAdReply: adon } }, { quoted: message });
 });
